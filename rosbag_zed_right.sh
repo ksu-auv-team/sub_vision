c299650f1777584bea606fa90a968ec98ffd0e36
Jeen225 <nejacquese@gmail.com>
2017-07-26 21:54:40 -0700

    change rosbag to record to jpeg buffer

--- rosbag_zed_right.sh
+++ rosbag_zed_right.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rosrun yolo compressed_repeater /right/image_rect_color/
-rosbag record -O zed_right /right/image_rect_color/compressed
+rosbag record -O zed_right /right/image_rect_color/jpegbuffer
